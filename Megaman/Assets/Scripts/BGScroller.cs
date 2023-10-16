69ccde14d4d1dffd3a790c5a81dda93e5546414c
James Araújo <jamison_k@hotmail.com>
2023-10-15 21:03:49 -0300

    Initial Commit

---
+++ Megaman/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+
+    public float speed = 0;
+    private Material mat;
+    private GameObject player;
+
+    private float pos = 0;
+
+    // Use this for initialization
+    void Start () {
+        mat = GetComponent<Renderer>().material;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        // Obtenha a velocidade atual do jogador.
+        float playerVelocityX = player.GetComponent<Rigidbody2D>().velocity.x;
+
+        // Verifique se o jogador está se movendo horizontalmente.
+        if (Mathf.Abs(playerVelocityX) > 0.1f) {
+            pos += speed * playerVelocityX * Time.deltaTime; // Use Time.deltaTime para suavizar o movimento.
+            mat.mainTextureOffset = new Vector2(pos, 0);
+        }
+    }
+}
